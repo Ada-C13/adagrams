8ede0dc2afa1d9119606871c18f14ee10809282b
Jocelyn Wang <wangjoc@gmail.com>
2020-02-11 15:40:09 -0800

    updated and clarified all comments

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,14 +1,20 @@
-#Game for user to create words.
+# Program that randomly displays 10 letters. 
+# Given those 10 letters, make words and recieve a score. 
+# Upon exiting, recieve file score and highest point word.
 
+# Access english dictionary
 require "csv"
 ENGLISH_DICT = CSV.read("assets/dictionary-english.csv")
 
 # Wave 1
-#Create hash with number of each tile available. Randomly sample 10.
+# Create hash with letters available and randomly sample 10 for user
 def draw_letters
   all_tiles = {
-    A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2,
-    N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+    A: 9, B: 2, C: 2, D: 4, E: 12,
+    F: 2, G: 3, H: 2, I: 9, J: 1,
+    K: 1, L: 4, M: 2, N: 6, O: 8,
+    P: 2, Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
   }
   tile_array = []
   all_tiles.each do |letter, number|
@@ -19,9 +25,9 @@ def draw_letters
   tile_array.sample(10)
 end
 
-#Wave 2
-#Confirms that the user input only uses ones that are available in their hand
-def uses_available_letters? (input, letters_in_hand) #doesn't address duplicated letters
+# Wave 2
+# Verifies that input is available for play
+def uses_available_letters? (input, letters_in_hand)
   input.split(//).each do |letter|
     if (letters_in_hand.include? letter.upcase) == false
       return false
@@ -31,8 +37,8 @@ def uses_available_letters? (input, letters_in_hand) #doesn't address duplicated
   true
 end
 
-#Wave 3
-#Calculates score for word based on point system and additional rule regarding words with more than 7 letters
+# Wave 3
+# Calculates score for word submitted handling special cases
 def score_word(word)
   score_hash = {
     A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
@@ -54,8 +60,8 @@ def score_word(word)
   return score_total
 end
 
-#Wave 4
-#Calculates the highest score from the words that were entered throughout this round
+# Wave 4
+# Calculates the highest score from submitted words
 def highest_score_from(words)
   high_score = 0
   high_word = ""
@@ -76,7 +82,7 @@ def highest_score_from(words)
 end
 
 # Wave 5
-#Checks to make sure user word is in the English dictionary
+# Checks for word to be in dictionary
 def is_in_english_dict?(input)
   return ENGLISH_DICT.include?([input.downcase])
 end
