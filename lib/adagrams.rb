f859c5c8f828887d3735675059a641316dbb47b8
dnguye2 <dnguyen1093@gmail.com>
2020-02-11 15:17:13 -0800

    wave 1 code completed

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,17 +1,6 @@
-# (1)
-# Draw 10 random letters
-
-# Base - draw_letters method
-# need to have a letter pool
-# generate the whole alphabet
-
-# print letter_pool
-letter = []
-letter = ('a'..'z').to_a
-quantity = [9, 2,	2, 4, 12, 2,	3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
-puts letter
-puts quantity.length
-
+# Creating letter pool
+letter = ('A'..'Z').to_a
+quantity = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1 ]
 
 def create_letter_pool(letter, quantity)
   index = 0
@@ -21,5 +10,28 @@ def create_letter_pool(letter, quantity)
   end
   return letter_pool
 end
+letter_pool_array = create_letter_pool(letter, quantity)
 
-print create_letter_pool(letter, quantity)
+# Wave (1)
+# Method to draw 10 random letters
+def draw_letters
+  # Welcome message
+    puts "Welcome to Adagrams!"
+    puts "Let's draw 10 letters from the letter pool..."
+    puts "You have drawn the letters:"
+    
+    letter = ('A'..'Z').to_a
+    quantity = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1 ]
+   
+  # Use the create_letter_pool method
+    create_letter_pool(letter, quantity)
+    letter_pool_array = create_letter_pool(letter, quantity)
+    
+  # Shuffle the letter pool
+    letter_pool_array = letter_pool_array.shuffle()
+    letters = letter_pool_array.sample(10) # Not sure if it will have more than 2 Cs
+    letter.join(', ')
+    return drawn_letters = letters
+end
+draw_letters
+print draw_letters.join(', ')
