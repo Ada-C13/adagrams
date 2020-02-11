1e3604742974c8f42f67c293ac67f848639d9aad
Jocelyn Wang <wangjoc@gmail.com>
2020-02-11 14:42:50 -0800

    completed initial draft of wave 3 - added in method score_word

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -27,3 +27,26 @@ def uses_available_letters? (input, letters_in_hand) #doesn't address duplicated
   end
   true
 end
+
+#Wave 3
+#Calculates score for word based on point system and additional rule regarding words with more than 7 letters
+def score_word(word)
+  score_hash = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+  }
+  score_total = 0
+  word.length.times do |i|
+    letter = word[i].upcase
+    score_total += score_hash[letter.to_sym]
+  end
+  if word.length >= 7 && word.length <= 10
+    score_total += 8
+  end
+  return score_total
+end
