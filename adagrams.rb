137a46b313adfc757a23866a857ce8589bb035e0
Alicia Combs <alicia.combs.92@gmail.com>
2020-02-12 09:19:58 -0800

    Added comments to explain code

--- adagrams.rb
+++ adagrams.rb
@@ -1,6 +1,9 @@
-# wave 1
+# Wave 1
+# Draws ten letters for the user.
 def draw_letters
+  # Variable holding our pool of letters.
   letters_pool = %w(A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z)
+  # Variable holding the letters passed to the user.
   hand = []
 
   10.times do
@@ -10,9 +13,12 @@ def draw_letters
   return hand
 end
 
-# wave 2
-def uses_available_letters?(input, letters_in_hand)
+# Wave 2
+# Checks if the word the user inputs is available in letters in hand.
+def uses_available_letters? (input, letters_in_hand)
+  # Variable holding array of letters of chosen word.
   letters = input.upcase.split('')
+  # Variable tracking if the word is available.
   is_in_hand = true
 
   letters.each do |letter|
@@ -24,9 +30,12 @@ def uses_available_letters?(input, letters_in_hand)
   return is_in_hand
 end
 
-# wave 3
+# Wave 3
+# Scores the word that the user has inputted.
 def score_word (word)
+  # Variable holding individual letters of chosen word.
   letters = word.upcase.split('')
+  # Variable tracking the score of the word.
   score = 0
 
   score_table = {
@@ -47,6 +56,8 @@ def score_word (word)
     end
   end
 
+  # If the chosen word is greater in length than 7 but less than 10,
+  # 8 more points will be added to their score.
   if letters.length >= 7 && letters.length <= 10
     score += 8
   end
@@ -54,16 +65,18 @@ def score_word (word)
   return score
 end
 
-# wave 4
+# Wave 4
+# Chooses the highest scoring word from all words inputted.
 def highest_score_from(words)
+  # Variable tracking each word and its respective score.
   words_hash = {}
 
   words.each do |word|
     words_hash[word] = score_word(word)
   end
 
-  highest_word = ""
-  highest_scored = 0
+  highest_word = "" # Variable tracking word that scores the highest.
+  highest_scored = 0 # Variable tracking the highest score.
 
   words_hash.each do |word, score|
     if score > highest_scored 
