a699b4426b7590cd5dabea0e5e934912a5ea39ba
Cathy O <cathyo@Adas-MacBook-Pro.local>
2020-02-12 15:59:07 -0800

    added and deleted comments for clarity

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,5 +1,6 @@
 require 'csv'
 
+# method to generate 10 random letters 
 def draw_letters
   letter_quantities = {
     'A': 9,
@@ -29,44 +30,45 @@ def draw_letters
     'Y': 2,
     'Z': 1
   }
-  pool_of_letters = []
+  pool_of_letters = [] 
+  # loop over letter_quantities hash to add the right quantity of each letter to the pool
   letter_quantities.each do |letter, quantities|
-    quantities.times do
+    quantities.times do 
       pool_of_letters.push(letter.to_s)
     end
   end 
-  chosen_letters = []
 
+  chosen_letters = [] 
+  # create an array of 10 random numbers to be used as indexes in the pool array 
   random_indexes_array = (0...pool_of_letters.length).to_a.sample(10)
 
+  # loop through the array of indexes and add the indexed element to chosen_letters
   random_indexes_array.each do |index|
     chosen_letters.push(pool_of_letters[index])
   end
 
-  # 10.times do
-  #   rand_index = rand(pool_of_letters.length)
-  #   chosen_letters.push(pool_of_letters[rand_index])
-  #   pool_of_letters.delete_at(rand_index)
-  # end
   return chosen_letters
 end
 
+# method to determine if the letters in the user input are in the array of chosen letters
 def uses_available_letters?(input, chosen_letters)
-  chosen_letters_array_to_manipulate = chosen_letters.clone
+  # create a clone of the chosen_letters array so the original does not get altered
+  chosen_letters_array_to_manipulate = chosen_letters.clone  
   input_split_letters = input.upcase.split("")
+  
+  # loop through each letter of user input and check if it's in the chosen_letters array
   input_split_letters.each do |letter|
     if chosen_letters_array_to_manipulate.include?(letter)
       chosen_letters_array_to_manipulate.delete_at(chosen_letters_array_to_manipulate.index(letter))
-      # puts "The letter testing is #{letter}"
-      # print chosen_letters_array_to_manipulate
-      # puts "\n"
     else
       return false
     end
   end
+
   return true
 end
 
+# method to calculate the score of a word
 def score_word(word)
   letter_values = {
     ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
@@ -79,6 +81,8 @@ def score_word(word)
   }
   letters_in_word = word.upcase.split("")
   total = 0
+ 
+  # for each letter, loop through the values hash and add the value of each letter to the total
   letters_in_word.each do |letter|
     letter_values.each do |letter_array, value|
       if letter_array.include?(letter)
@@ -86,15 +90,19 @@ def score_word(word)
       end    
     end
   end
+
   if word.length > 6
     total += 8
   end
+
   return total
 end
 
+# method to determine which word has the highest score
 def highest_score_from(words)
   word_with_highest_score = ""
   highest_score = 0
+ 
   words.each do |word|
     score_of_word = score_word(word)
     if score_of_word > highest_score
@@ -114,6 +122,7 @@ def highest_score_from(words)
   return {:word => word_with_highest_score, :score => highest_score}
 end
 
+# method to determine if word is in the english dictionary
 def is_in_english_dict?(input)
   csv_data = CSV.parse(File.read("assets/dictionary-english.csv"), headers: true).by_col[0]
   return csv_data.include?(input.downcase)
