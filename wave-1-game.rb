5929e24b6c7a9976520f99e26a59bb3bef19ded0
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-02-13 08:56:46 -0800

    wave 2 is ready yeahhhhhh

--- wave-1-game.rb
+++ wave-1-game.rb
@@ -10,9 +10,29 @@ def display_drawn_letters(letters)
   puts letters.join(', ')
 end
 
-def run_game
-  display_welcome_message
-  display_drawn_letters(draw_letters)
-end
-
+def draw_letters 
+  all_letters = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2 , "X": 1, "Y": 2, "Z": 1}
+  
+    letter_bank = []
+    all_letters.each do |letter, n|
+      n.times do 
+        letter_bank << letter.to_s
+      end 
+    end 
+    hand_letters = letter_bank.sample(10)
+    return hand_letters
+    
+  end 
+  
+  
+  def run_game
+    display_welcome_message
+    display_drawn_letters(draw_letters)
+  end
 run_game
+
+#we need a way to store the letters 
+
+
+  
+  
