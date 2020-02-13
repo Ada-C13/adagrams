2e335ab4b009f8497370bd841d88bf32c571e3e2
Katie <katie.vandervoort@gmail.com>
2020-02-13 15:23:33 -0800

    changed letter quantities into a constant to move outside of the method

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,38 +1,39 @@
 require 'csv'
 
+LETTER_QUANTITIES = {
+  'A': 9,
+  'B': 2,
+  'C': 2,
+  'D': 4,
+  'E': 12,
+  'F': 2,
+  'G': 3,
+  'H': 2, 
+  'I': 9,
+  'J': 1,
+  'K': 1,
+  'L': 4,
+  'M': 2,
+  'N': 6,
+  'O': 8,
+  'P': 2,
+  'Q': 1,
+  'R': 6,
+  'S': 4,
+  'T': 6,
+  'U': 4,
+  'V': 2,
+  'W': 2,
+  'X': 1,
+  'Y': 2,
+  'Z': 1
+}
+
 # method to generate 10 random letters 
 def draw_letters
-  letter_quantities = {
-    'A': 9,
-    'B': 2,
-    'C': 2,
-    'D': 4,
-    'E': 12,
-    'F': 2,
-    'G': 3,
-    'H': 2, 
-    'I': 9,
-    'J': 1,
-    'K': 1,
-    'L': 4,
-    'M': 2,
-    'N': 6,
-    'O': 8,
-    'P': 2,
-    'Q': 1,
-    'R': 6,
-    'S': 4,
-    'T': 6,
-    'U': 4,
-    'V': 2,
-    'W': 2,
-    'X': 1,
-    'Y': 2,
-    'Z': 1
-  }
   pool_of_letters = [] 
-  # loop over letter_quantities hash to add the right quantity of each letter to the pool
-  letter_quantities.each do |letter, quantities|
+  # loop over LETTER_QUANTITIES hash to add the right quantity of each letter to the pool
+  LETTER_QUANTITIES.each do |letter, quantities|
     quantities.times do 
       pool_of_letters.push(letter.to_s)
     end
