eaa724a2ea7554d4200c9440b924ace369138da8
Maryam Heshmati <mairheshmati@gmail.com>
2020-02-14 16:32:13 -0800

    completed wave4, 5. will calculated the highest score word and analyze if the word is an English word from pulling data from the dictionary

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -30,14 +30,17 @@ def draw_letters
     "y" => 2,
     "z" => 1,
   }
+  #create an array with all the availble letters
   pool_letters = []
   sample.each do |letter, quantity|
     quantity.times do
       pool_letters << letter
     end
   end
+
   #randomize the pool of letters
   pool_letters = pool_letters.sample(pool_letters.length)
+
   #display the 10 letters they have drawn
   return pool_letters.first(10)
 end
@@ -45,25 +48,19 @@ end
 #wave 2
 def uses_available_letters?(input, letters_in_hand)
   input_splitted = input.split ""
-  
+  #make a copy of letters in hand so it remains un-changed
+  #[].replace will replace the contents of selected_letters array which is empty with the contents from letters_in_hand
   selected_letters = [].replace(letters_in_hand)
-
   input_splitted.each do |letter|
     if selected_letters.include? letter
-      # selected_letters << letter
-      where_is_my_letter = selected_letters.find_index(letter)
-      selected_letters.delete_at(where_is_my_letter)
+      where_is_my_letter = selected_letters.find_index(letter) #using .find_index to see where that letter from the word is in the array that contain the 10 availble letters
+      selected_letters.delete_at(where_is_my_letter) #modify the duplicated array (selected_letters) to remove that item from the array, this will ensure that only the letter with that index is removed instead of all the same-letters in the hand
     else
       return false
     end
   end
   return true
 end
-
-uses_available_letters?("apple", ["a", "p", "a", "p", "e", "l", "z", "k", "a", "a"])
-
-
-
 
 #wave 3
 def score_word(word)
@@ -76,14 +73,12 @@ def score_word(word)
     "8" => %w(J X),
     "10" => %w(Q Z),
   }
+  #converting the word into upcase so it would be independent of the users input
   word_splitted = word.upcase.split ""
-
   total_score = 0
   word_splitted.each do |letter|
     score.each do |value, letters_with_value|
-      if letters_with_value.include? letter
-        total_score = total_score + value.to_i
-      end
+      total_score += value.to_i if letters_with_value.include? letter #add the associated value  with that letter (converted to integer) to the total score
     end
   end
   if word_splitted.length >= 7 && word_splitted.length <= 10
@@ -96,42 +91,42 @@ end
 def highest_score_from(words)
   word_and_score = {}
   words.each do |word|
-    word_and_score[word] = score_word(word)
+    word_and_score[word] = score_word(word) #calling the method score_word to determine the score of that word
   end
 
   highest_score = 0
   highest_scored_name = nil
   word_and_score.each do |word, score|
-    if score > highest_score
+    if score > highest_score #determine the word with the highest score
       highest_score = score
       highest_scored_name = word
     end
 
-    if highest_score == score
-      if highest_scored_name.length < 10 && word.length < 10 && highest_scored_name.length > word.length
+    if highest_score == score #in case there is a tie of scores
+      if highest_scored_name.length < 10 && word.length < 10 && highest_scored_name.length > word.length #if the length of the word is less than 10 letters, the word with the least letters will win
         highest_scored_name = word
-      elsif (highest_scored_name.length >= 10 || word.length >= 10) && word.length > highest_scored_name.length
+      elsif (highest_scored_name.length >= 10 || word.length >= 10) && word.length > highest_scored_name.length #unless a word is 10 letters or longer, the that word will be the winner
         highest_scored_name = word
       end
     end
   end
-  winning_word_score = {}
-  winning_word_score[:word] = highest_scored_name
-  winning_word_score[:score] = highest_score
+  winning_word_score = {
+    word: highest_scored_name,
+    score: highest_score
+  }
   return winning_word_score
 end
 
 #wave 5
 require "csv"
-require "awesome_print"
 
+#requiring the English dictionary that is in a CSV file to see if the user's input is an actual English word
 def is_in_english_dict?(input)
-  words = CSV.read("./assets/dictionary-english.csv").each do |word|
+  words = CSV.read("./assets/dictionary-english.csv")
+  words.each do |word|
     if word.include? input
       return true
     end
   end
   return false
 end
-
-
